d751d65d39030387ea966ea9704e47576fb8c0bc
elvis-ndubuisi <provictor.ie@gmail.com>
2024-04-29 06:14:23 +0100

    refactor workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  ignorePatterns: ['apps/**', 'packages/**', 'toolings/**'],
+  ignorePatterns: ['apps/**', 'packages/**', 'toolings/**', 'internal/**'],
   extends: ['@tooling/eslint-config/library.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
