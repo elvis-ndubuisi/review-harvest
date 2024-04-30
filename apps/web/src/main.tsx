30fbf8f07e37fa8718fd7b168d41ecb08ced7ee4
elvis-ndubuisi <provictor.ie@gmail.com>
2024-04-30 03:25:14 +0100

    init web end

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
 import './index.css'
+import {RouterProvider, createRouter} from '@tanstack/react-router'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>{/* <App /> */}</React.StrictMode>,
-)
+import {routeTree} from './routeTree.gen'
+const router = createRouter({routeTree})
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+}
